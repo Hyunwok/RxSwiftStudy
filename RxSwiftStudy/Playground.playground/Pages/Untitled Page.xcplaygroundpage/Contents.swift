3e8a088027d8025486ccba83c9cf28796f2fb133
이현욱 <hjhsha827@gmail.com>
2020-07-16 23:50:17 +0900

    [Fix] 코드 수정

--- RxSwiftStudy/Playground.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
+++ RxSwiftStudy/Playground.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
@@ -1,13 +1,16 @@
+
 //: A UIKit based Playground for presenting user interface
 
 import UIKit
 import RxSwift
 
+let disposeBag = DisposeBag()
 
-let a = Variable(1)
-let b = Variable(2)
+let subject = PublishSubject<String>()
 
-let c = Observable.combineLatest(a.asObservable(), b.asObservable()) { $0 + $1 }
-    .filter{ $0 > 0 }
-    .map { "\($0) is abc"}
-c.subscribe(onNext: { print($0) })
+subject.onNext("전달되지 않을 onNext")
+
+let ob1 = subject.subscribe { print("지금부터 구독 시작", $0)}
+ob1.disposed(by: disposeBag)
+
+subject.onNext("PublishSubject는 subscribe 전에 onNext는 전달해주지 않음, 구독 이후에 것만 전달해줌")
