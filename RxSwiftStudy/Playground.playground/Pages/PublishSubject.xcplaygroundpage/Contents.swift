773c1d78338608570c0daf67fafc72dcc8fca860
이현욱 <hjhsha827@gmail.com>
2020-07-17 09:45:57 +0900

    [Fix] 코드 수정

--- RxSwiftStudy/Playground.playground/Pages/PublishSubject.xcplaygroundpage/Contents.swift
+++ RxSwiftStudy/Playground.playground/Pages/PublishSubject.xcplaygroundpage/Contents.swift
@@ -1,6 +1,3 @@
-
-//: A UIKit based Playground for presenting user interface
-
 import UIKit
 import RxSwift
 
@@ -14,3 +11,10 @@ let ob1 = subject.subscribe { print("지금부터 구독 시작", $0)}
 ob1.disposed(by: disposeBag)
 
 subject.onNext("PublishSubject는 subscribe 전에 onNext는 전달해주지 않음, 구독 이후에 것만 전달해줌")
+
+//subject.onCompleted()
+subject.onError(Error)
+
+let ob2 = subject.subscribe {print("2번째 옵저버로 completed 이후에는 onNext 불가")}
+ob2.disposed(by: disposeBag)
+
