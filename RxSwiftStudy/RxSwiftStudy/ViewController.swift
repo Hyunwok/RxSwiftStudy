b79d581f13805f86ec8ffe7bf2e114ff49d7a2ef
이현욱 <hjhsha827@gmail.com>
2020-07-18 08:58:09 +0900

    [Add] Label과 코드 추가

--- RxSwiftStudy/RxSwiftStudy/ViewController.swift
+++ RxSwiftStudy/RxSwiftStudy/ViewController.swift
@@ -6,9 +6,10 @@ class ViewController: UIViewController {
     
     let loginModel = LoginViewModel()
     let disposeBag = DisposeBag()
-
+    
     @IBOutlet weak var loginIdTxtField: UITextField!
     @IBOutlet weak var loginPwTxtField: UITextField!
+    @IBOutlet weak var enableLbl: UILabel!
     @IBOutlet weak var LoginBtn: UIButton!
     
     override func viewDidLoad() {
@@ -19,11 +20,14 @@ class ViewController: UIViewController {
         
         loginModel.isValid().bind(to: LoginBtn.rx.isEnabled).disposed(by: disposeBag)
         loginModel.isValid().map{$0 ? 1 : 0.1}.bind(to: LoginBtn.rx.alpha).disposed(by: disposeBag)
-            
+        
+        loginModel.isValid().subscribe(onNext:{ [weak self] text in
+            self?.enableLbl.text = text ? "Able" : "Enable"
+        })
         
         // Do any additional setup after loading the view.
     }
-
+    
     @IBAction func getLogin(_ sender: UIButton) {
         print("tabbed")
     }
