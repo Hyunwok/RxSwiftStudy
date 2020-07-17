88a4dd1ceb812ef73bf03e70caa71f12740217b8
이현욱 <hjhsha827@gmail.com>
2020-07-17 23:46:38 +0900

    [Fix] Error 추가

--- RxSwiftStudy/Playground.playground/Pages/PublishSubject.xcplaygroundpage/Contents.swift
+++ RxSwiftStudy/Playground.playground/Pages/PublishSubject.xcplaygroundpage/Contents.swift
@@ -1,9 +1,12 @@
 import UIKit
 import RxSwift
 
+enum MyError: Error {
+    case error
+}
 let disposeBag = DisposeBag()
 
-let subject = PublishSubject<String>()
+let subject = BehaviorSubject<String>()
 
 subject.onNext("전달되지 않을 onNext")
 
@@ -13,8 +16,9 @@ ob1.disposed(by: disposeBag)
 subject.onNext("PublishSubject는 subscribe 전에 onNext는 전달해주지 않음, 구독 이후에 것만 전달해줌")
 
 //subject.onCompleted()
-subject.onError(Error)
+subject.onError(MyError.error)
 
 let ob2 = subject.subscribe {print("2번째 옵저버로 completed 이후에는 onNext 불가")}
 ob2.disposed(by: disposeBag)
+
 
