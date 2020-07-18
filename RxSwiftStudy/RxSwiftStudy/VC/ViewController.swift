0321e95fb8fd1c08470f8ef1095ac1fbb8a84e14
이현욱 <hjhsha827@gmail.com>
2020-07-18 22:50:16 +0900

    [Fix] code 수정

--- RxSwiftStudy/RxSwiftStudy/VC/ViewController.swift
+++ RxSwiftStudy/RxSwiftStudy/VC/ViewController.swift
@@ -19,11 +19,12 @@ class ViewController: UIViewController {
         loginPwTxtField.rx.text.map { $0 ?? "" }.bind(to: loginModel.pwPublishSubject).disposed(by: disposeBag)
         
         loginModel.isValid().bind(to: LoginBtn.rx.isEnabled).disposed(by: disposeBag)
-        loginModel.isValid().map{$0 ? 1 : 0.1}.bind(to: LoginBtn.rx.alpha).disposed(by: disposeBag)
+        loginModel.isValid().map{$0 ? 1 : 0.5}.bind(to: LoginBtn.rx.alpha).disposed(by: disposeBag)
         
         loginModel.isValid().subscribe(onNext:{ [weak self] text in
             self?.enableLbl.text = text ? "Able" : "Enable"
-        })
+            self?.enableLbl.textColor = text ? .green : .red
+            }).disposed(by: disposeBag)
         
         // Do any additional setup after loading the view.
     }
