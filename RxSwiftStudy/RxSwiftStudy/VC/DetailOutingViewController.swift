3967c4826cd3d78ff8454ea8c745957c73079cd4
이현욱 <hjhsha827@gmail.com>
2020-07-20 23:27:03 +0900

    [Add] DetailOutingViewController

---
+++ RxSwiftStudy/RxSwiftStudy/VC/DetailOutingViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailOutingViewController.swift
+//  RxSwiftStudy
+//
+//  Created by 이현욱 on 2020/07/20.
+//  Copyright © 2020 이현욱. All rights reserved.
+//
+
+import UIKit
+
+class DetailOutingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
