8e13b0950e1d032f6574fb2f9ca0c1fb4bc56c1f
이현욱 <hjhsha827@gmail.com>
2020-08-22 23:33:57 +0900

    [Add] 코드 추가

--- RxSwiftStudy/RxSwiftStudy/Extension.swift
+++ RxSwiftStudy/RxSwiftStudy/Extension.swift
@@ -7,3 +7,32 @@
 //
 
 import Foundation
+import UIKit
+
+extension UIViewController {
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        textField.textColor = .cyan
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        textField.textColor = .gray
+    }
+    
+    open override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?){
+        self.view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        textField.textColor = .cyan
+        return true
+    }
+    
+    @objc func keyboardWillShow(_ sender: Notification) {
+        self.view.frame.origin.y = -150
+    }
+    
+    @objc func keyboardWillDisa(_ sender: Notification) {
+        self.view.frame.origin.y = 0
+    }
+}
